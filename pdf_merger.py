e45bc0e5578d6ee59b3f3c5a801f09d8e1a0bb60
ThinhLe881 <thinhleminh201@gmail.com>
2022-07-21 01:53:39 -0400

    User can choose source dir and dest dir

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,11 +1,22 @@
 import PyPDF2
 import os
+from sys import argv
+
+src_path = argv[1]
+dest_path = argv[2]
+
+print(src_path)
+print(dest_path)
 
 merger = PyPDF2.PdfFileMerger()
 
-for file in os.listdir(os.curdir):
-    if file.endswith(".pdf"):
-        merger.append(file)
+directory = os.fsencode(src_path)
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith('.pdf'):
+        merger.append(os.path.join(src_path, filename))
 
-merger.write("merged.pdf")
+filename = dest_path + './' + 'merged.pdf'
+merger.write(filename)
 merger.close()
+print('Merge complete')
