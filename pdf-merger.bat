438ade2fb00e18e0531eebda65f72eb1d3a943a7
ThinhLe881 <thinhleminh201@gmail.com>
2022-12-17 14:44:07 -0500

    change parser

--- pdf-merger.bat
+++ pdf-merger.bat
@@ -1,5 +1,5 @@
 @echo off
-set src_dir=""
-set dest_dir=""
-cd /d ""
+set src_dir=" -the directory where the pdfs are placed- "
+set dest_dir=" -the directory where the merged pdf will be placed- "
+cd /d " -the directory of the python file- "
 python pdf_merger.py %src_dir% %dest_dir%
--- pdf_merger.py
+++ pdf_merger.py
@@ -1,20 +1,38 @@
+import argparse
 import PyPDF2
 import os
-from sys import argv
 
-src_path = argv[1]
-dest_path = argv[2]
+def main():
+    parser = argparse.ArgumentParser(description='YouTube Video Downloader')
+    parser.add_argument('src_path', metavar='src_path', type=str, help='The directory where the pdfs are placed')
+    parser.add_argument('dest_path', metavar='dest_path', type=str, help='The directory where the merged pdf will be placed')
 
-merger = PyPDF2.PdfFileMerger()
+    args = parser.parse_args()
 
-directory = os.fsencode(src_path)
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith('.pdf'):
-        merger.append(os.path.join(src_path, filename))
+    src_path = args.src_path
+    dest_path = args.dest_path
 
-filename = dest_path + './' + 'merged.pdf'
-merger.write(filename)
-merger.close()
-print('Save as: ' + dest_path)
-print('Merge complete')
+    merger = PyPDF2.PdfFileMerger()
+
+    count = 0
+
+    directory = os.fsencode(src_path)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith('.pdf'):
+            merger.append(os.path.join(src_path, filename))
+            count += 1
+
+    if count == 0:
+        print('No pdf file found')
+        exit(1)
+
+    filename = rf'{dest_path}/Merged.pdf'
+    merger.write(filename)
+    merger.close()
+
+    print('Save as: ' + dest_path)
+    print('Done')
+
+if __name__ == '__main__':
+    main()
